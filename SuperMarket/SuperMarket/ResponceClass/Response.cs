54982ab53957af7fd2b55aad45606ff7ccb935fe
santhoshidosapati <dosapatisanthu@gmail.com>
2014-07-23 13:03:09 +0530

    TEst

---
+++ SuperMarket/SuperMarket/ResponceClass/Response.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperMarket.ResponceClass
+{
+    public class Response
+    {
+        /// <summary>
+        /// 5555 for success.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// User friendly string
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Generally this will be a Stack Trace
+        /// </summary>
+        public string ExtendedMessage { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="intId"></param>
+        /// <param name="strMessage"></param>
+        public Response(int intId, string strMessage)
+        {
+            this.Id = intId;
+            this.Message = strMessage;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="intId"></param>
+        /// <param name="strMessage"></param>
+        /// <param name="strExtendedMessage"></param>
+        public Response(int intId, string strMessage, string strExtendedMessage)
+        {
+            this.Id = intId;
+            this.Message = strMessage;
+            this.ExtendedMessage = strExtendedMessage;
+        }
+    }
+}
