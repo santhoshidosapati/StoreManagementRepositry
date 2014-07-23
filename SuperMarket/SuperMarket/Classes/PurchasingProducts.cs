157737fba0fbb7651b44b599d350d796a557b238
santhoshidosapati <dosapatisanthu@gmail.com>
2014-07-23 13:00:41 +0530

    Added classes

---
+++ SuperMarket/SuperMarket/Classes/PurchasingProducts.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using SQLConnector;
+using System.Data;
+using SuperMarket.ResponceClass;
+
+
+namespace SuperMarket.Classes
+{
+    public class PurchasingProducts
+    {
+        const string SUCCESS_MSG = "Successfully {0} a PurchasingProducts";
+        const string Failure_MSG = "Unable to {0} a PurchasingProducts";
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { set; get; }
+        public decimal Price { set; get; }
+
+
+        public Response Create(PurchasingProducts objPurchasingProducts)
+        {
+            SQLConnector.DBOperations objdb = new DBOperations(Properties.Settings.Default.SuperMarketConnectionString);
+            string stInsert = @"INSERT INTO [PurchasingProducts]([Name],[Quantity] ,[Price])
+                VALUES(@Name,@Quantity,@Price)";
+            string[] strAddParameterName = new string[] { "Name", "Quantity", "Price" };
+            object[] objAddparametervalue = new object[] { objPurchasingProducts.Name, objPurchasingProducts.Quantity, objPurchasingProducts.Price };
+            if (!objdb.ExecuteQuery(stInsert, strAddParameterName, objAddparametervalue))
+                return new Response(9999, string.Format(Failure_MSG, "insert"));
+
+            else
+
+                return new Response(5555, string.Format(SUCCESS_MSG, "inserted"));
+        }
+
+        public Response Delete(PurchasingProducts objPurchasingProducts)
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.SuperMarketConnectionString);
+            string strDelete = "DELETE FROM [PurchasingProducts] WHERE Id=@Id";
+            string[] strAddParameterName = new string[] { "Id" };
+            object[] objAddparametervalue = new object[] { objPurchasingProducts.Id };
+            if (!objdb.ExecuteQuery(strDelete, strAddParameterName, objAddparametervalue))
+
+                return new Response(9999, string.Format(Failure_MSG, "delete"));
+            else
+
+                return new Response(5555, string.Format(SUCCESS_MSG, "deleted"));
+        }
+
+        public Response Update(PurchasingProducts objPurchasingProducts)
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.SuperMarketConnectionString);
+            string strUpsate = @"UPDATE [PurchasingProducts]
+                  SET [Name] = @Name, 
+                           [Quantity] = @Quantity,
+                     [Price] = @Price
+                WHERE Id=@Id";
+            string[] strAddParameterName = new string[] { "Name", "Quantity", "Price", "Id" };
+            object[] objAddparametervalue = new object[] { objPurchasingProducts.Name, objPurchasingProducts.Quantity, objPurchasingProducts.Price, objPurchasingProducts.Id };
+            if (!objdb.ExecuteQuery(strUpsate, strAddParameterName, objAddparametervalue))
+                return new Response(9999, string.Format(Failure_MSG, "update"));
+            else
+                return new Response(5555, string.Format(SUCCESS_MSG, "updated"));
+        }
+
+        public static PurchasingProducts Get(int Id)
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.SuperMarketConnectionString);
+
+            PurchasingProducts objPurchasingProducts = new PurchasingProducts();
+            string strGetOneRecord = "SELECT * FROM [PurchasingProducts] where Id=@Id";
+            string[] strAddParameterName = new string[] { "Id" };
+            object[] objAddparametervalue = new object[] { objPurchasingProducts.Id };
+            System.Data.DataTable objtab = objdb.ExecuteGetAllQuery(strGetOneRecord, strAddParameterName, objAddparametervalue);
+            return objPurchasingProducts;
+        }
+
+        public static List<PurchasingProducts> GetAll()
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.SuperMarketConnectionString);
+            List<PurchasingProducts> lstPurchasingProducts = new List<PurchasingProducts>();
+            PurchasingProducts objPurchasingProducts = new PurchasingProducts();
+
+            string strGetOneRecord = "SELECT * FROM [PurchasingProducts]";
+            string[] strAddParameterName = new string[] { };
+            object[] objAddparametervalue = new object[] { };
+            DataTable objtab = new DataTable();
+            objtab = objdb.ExecuteGetAllQuery(strGetOneRecord, strAddParameterName, objAddparametervalue);
+
+            for (int i = 0; i < objtab.Rows.Count; i++)
+            {
+                int intId;
+                int intQuantity;
+                decimal decPrice;
+                objPurchasingProducts = new PurchasingProducts();
+                int.TryParse(objtab.Rows[i]["Id"].ToString(), out intId);
+                objPurchasingProducts.Id = intId;
+                objPurchasingProducts.Name = objtab.Rows[i]["Name"] != null ? objtab.Rows[i]["Name"].ToString() : string.Empty;
+                int.TryParse(objtab.Rows[i]["Quqntity"].ToString(), out intQuantity);
+                objPurchasingProducts.Quantity = intQuantity;
+                decimal.TryParse(objtab.Rows[i]["Price"].ToString(), out decPrice);
+                objPurchasingProducts.Price = decPrice;
+                lstPurchasingProducts.Add(objPurchasingProducts);
+            }
+            return lstPurchasingProducts;
+        }
+
+    }
+}
---
+++ SuperMarket/SuperMarket/Classes/Transaction.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using SQLConnector;
+using System.Data;
+using SuperMarket.ResponceClass;
+
+namespace SuperMarket.Classes
+{
+    public class Transaction
+    {
+        const string SUCCESS_MSG = "Successfully {0}  Transaction";
+        const string Failure_MSG = "Unable to {0} a Transaction";
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { set; get; }
+        public int Quqntity { set; get; }
+        public decimal Profit { set; get; }
+
+
+        public Response Create(Transaction objTransaction)
+        {
+            SQLConnector.DBOperations objdb = new DBOperations(Properties.Settings.Default.SuperMarketConnectionString);
+            string stInsert = @"INSERT INTO [Transaction] ([Name],[Date],[Quqntity] ,[Profit])
+             VALUES (@Name, @Date, @Quqntity, @Profit)";
+            string[] strAddParameterName = new string[] { "Name", "Date", "Quqntity", "Profit" };
+            object[] objAddparametervalue = new object[] { objTransaction.Name, objTransaction.Date, objTransaction.Quqntity, this.Profit };
+            if (!objdb.ExecuteQuery(stInsert, strAddParameterName, objAddparametervalue))
+                return new Response(9999, string.Format(Failure_MSG, "insert"));
+
+            else
+
+                return new Response(5555, string.Format(SUCCESS_MSG, "inserted"));
+        }
+
+       public Response Delete(Transaction objTransaction)
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.SuperMarketConnectionString);
+            string strDelete = "DELETE FROM [UsersTable] WHERE Id=@Id";
+            string[] strAddParameterName = new string[] { "Id" };
+            object[] objAddparametervalue = new object[] { objTransaction.Id };
+            if (!objdb.ExecuteQuery(strDelete, strAddParameterName, objAddparametervalue))
+
+                return new Response(9999, string.Format(Failure_MSG, "delete"));
+            else
+
+                return new Response(5555, string.Format(SUCCESS_MSG, "deleted"));
+        }
+
+        public Response Update(Transaction objTransaction)
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.SuperMarketConnectionString);
+            string strUpsate = @"UPDATE [SuperMarket].[dbo].[Transaction]
+               SET [Name] = @Name
+                  ,[Date] = @Date, date,>
+                  ,[Quqntity] = @Quqntity, int,>
+                  ,[Profit] = @Profit, money,>
+             WHERE Id=@Id ";
+            string[] strAddParameterName = new string[] { "Name", "Date", "Quqntity","Profit","Id" };
+            object[] objAddparametervalue = new object[] { objTransaction.Name, objTransaction.Date, objTransaction.Quqntity ,objTransaction.Profit,objTransaction.Id};
+            if (!objdb.ExecuteQuery(strUpsate, strAddParameterName, objAddparametervalue))
+                return new Response(9999, string.Format(Failure_MSG, "update"));
+            else
+                return new Response(5555, string.Format(SUCCESS_MSG, "updated"));
+        }
+
+        public static UsersTable Get(int Id)
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.SuperMarketConnectionString);
+            UsersTable objUsersTable = new UsersTable();
+            string strGetOneRecord = "SELECT * FROM [Transaction] where Id=@Id";
+            string[] strAddParameterName = new string[] { "Id" };
+            object[] objAddparametervalue = new object[] { objUsersTable.Id };
+            System.Data.DataTable objtab = objdb.ExecuteGetAllQuery(strGetOneRecord, strAddParameterName, objAddparametervalue);
+            return objUsersTable;
+        }
+
+        public static List<Transaction> GetAll()
+        {
+            DBOperations objdb = new DBOperations(Properties.Settings.Default.SuperMarketConnectionString);
+            List<Transaction> lstTransaction = new List<Transaction>();
+            Transaction objTransaction = new Transaction();
+            string strGetOneRecord = "SELECT * FROM [Transaction]";
+            string[] strAddParameterName = new string[] { };
+            object[] objAddparametervalue = new object[] { };
+            DataTable objtab = new DataTable();
+            objtab = objdb.ExecuteGetAllQuery(strGetOneRecord, strAddParameterName, objAddparametervalue);
+
+            for (int i = 0; i < objtab.Rows.Count; i++)
+            {
+                int intId;
+                DateTime dtDate;
+                int intQuqntity;
+                decimal decProfit;
+                objTransaction = new Transaction();
+                int.TryParse(objtab.Rows[i]["Id"].ToString(), out intId);
+                objTransaction.Id = intId;
+                objTransaction.Name = objtab.Rows[i]["Name"] != null ? objtab.Rows[i]["Name"].ToString() : string.Empty;
+                DateTime.TryParse(objtab.Rows[i]["Date"].ToString(), out dtDate);
+                objTransaction.Date = dtDate;
+                int.TryParse(objtab.Rows[i]["Quantity"].ToString(), out intQuqntity);
+                objTransaction.Quqntity = intQuqntity;
+                decimal.TryParse(objtab.Rows[i]["Profit"].ToString(), out decProfit);
+                objTransaction.Profit = decProfit;              
+                lstTransaction.Add(objTransaction);
+            }
+            return lstTransaction;
+        }
+    }
+}
