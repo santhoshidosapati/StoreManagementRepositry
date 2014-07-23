f3b53c452c5e47ca49bc58947181cf6a82698ae7
ManojkumarBejjarapu <b.manojkumar473@gmail.com>
2014-07-23 12:54:43 +0530

    First

---
+++ SuperMarket/SuperMarket/Js/Example.js
@@ -0,0 +1,50 @@
+﻿$(document).ready(function () {
+    
+    $(".button").button();
+
+    $(".chosen").chosen();
+    $("#tabs").tabs();
+    $(".Dialog").dialog(
+    {
+        dialogClass: "no-close",
+        buttons:
+        {
+
+            "clear": clear,
+            "Cancel": close
+        }
+
+
+    }
+    )
+    $("#btnSubmit").click(function () {
+        var id = $("#txtId").val();
+        var item = $(".chosen").val();
+        var quant = $("#txtQuant");
+        var price = $("#txtPrice").val();
+        $(".Dialog").dialog("close");
+
+
+    })
+    $("#btnSale").click(function () {
+
+        $(".Dialog").dialog({
+            autoOpen: true
+        });
+
+    })
+
+
+
+
+});
+function clear() {
+    $("#txtQuant").val("");
+
+
+}
+function close() {
+
+    $(".Dialog").dialog("close");
+}
+
